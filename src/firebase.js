d6215a76da73db875ca14b8a3bb33b0f0a078f5d
nisha2714 <141358780+nisha2714@users.noreply.github.com>
2024-09-30 13:57:16 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,17 +4,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlotIbvlspHNJjnhhtvsk6w02Ry5fwLJI",
-  authDomain: "to-do-list-9b8e8.firebaseapp.com",
-  projectId: "to-do-list-9b8e8",
-  storageBucket: "to-do-list-9b8e8.appspot.com",
-  messagingSenderId: "448568539831",
-  appId: "1:448568539831:web:f1e064b3ebfc8a6a746b32",
-  measurementId: "G-6B1LNDFF2K",
-  databaseURL:"https://to-do-list-9b8e8.firebaseio.com/"
-
+  apiKey: "AIzaSyA0AynpE9eKAfBNGAhEbbSbUuz2_t6Nt-s",
+  authDomain: "react-task-6.firebaseapp.com",
+  projectId: "react-task-6",
+  storageBucket: "react-task-6.appspot.com",
+  messagingSenderId: "1049453818644",
+  appId: "1:1049453818644:web:468fb2acd2b24e50978801",
+  measurementId: "G-6XBXZZX5H0",
+  databaseURL: "https://react-task-6-default-rtdb.firebaseio.com",
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
